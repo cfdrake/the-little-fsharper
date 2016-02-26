9bd70cb18676c04addb8b886606cd540830de234
Colin Drake <colin.f.drake@gmail.com>
2016-02-26 17:16:50 -0500

    Don't need to pattern match and bind on unused values

--- Chapter02.fsx
+++ Chapter02.fsx
@@ -12,7 +12,7 @@ let rec only_onions (s) =
   match s with
   | Skewer -> true
   | Onion(x) -> only_onions x
-  | Lamb(x) -> false
-  | Tomato(x) -> false
+  | Lamb(_) -> false
+  | Tomato(_) -> false
 
 (only_onions : ShishKebab -> bool)
