43dcc370e4b7271d6758c04921839bab53d05662
Colin Drake <colin.f.drake@gmail.com>
2016-02-26 17:54:12 -0500

    Adhere to apparent F# naming conventions

--- Chapter02.fsx
+++ Chapter02.fsx
@@ -8,21 +8,21 @@ type ShishKebab =
   | Tomato of ShishKebab
 
 // 15
-let rec only_onions (s) =
+let rec onlyOnions (s) =
   match s with
   | Skewer -> true
-  | Onion(x) -> only_onions x
+  | Onion(x) -> onlyOnions x
   | Lamb(_) -> false
   | Tomato(_) -> false
 
-(only_onions : ShishKebab -> bool)
+(onlyOnions : ShishKebab -> bool)
 
 // 63
-let rec is_vegetarian (s) = 
+let rec isVegetarian (s) = 
   match s with
   | Skewer -> true
-  | Onion(x) -> is_vegetarian x
+  | Onion(x) -> isVegetarian x
   | Lamb(_) -> false
-  | Tomato(x) -> is_vegetarian x
+  | Tomato(x) -> isVegetarian x
 
-(is_vegetarian : ShishKebab -> bool)
+(isVegetarian : ShishKebab -> bool)
