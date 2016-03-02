db3ffc8c14573d48e3cee7f82e371b5396b0ae81
Colin Drake <colin.f.drake@gmail.com>
2016-03-02 17:58:29 -0500

    Work out 28-57, Ch 05

--- Chapter05.fsx
+++ Chapter05.fsx
@@ -27,4 +27,34 @@ let rec removeTuna p =
   | Topping(t, p) -> Topping(t, removeTuna(p))
 
 // 28
-// ...
+let rec removeFish = function
+  | (_, Bottom) -> Bottom
+  | (Anchovy, Topping(Anchovy, p)) -> removeFish(Anchovy, p)
+  | (Anchovy, Topping(t, p)) -> Topping(t, removeFish(Anchovy, p))
+  | (Lox, Topping(Lox, p)) -> removeFish(Lox, p)
+  | (Lox, Topping(t, p)) -> Topping(t, removeFish(Lox, p))
+  | (Tuna, Topping(Tuna, p)) -> removeFish(Tuna, p)
+  | (Tuna, Topping(t, p)) -> Topping(t, removeFish(Tuna, p))
+
+(removeFish : (Fish * FishPizza) -> FishPizza)
+
+// 38
+let eqFish = function
+  | (Anchovy, Anchovy) -> true
+  | (Lox, Lox) -> true
+  | (Tuna, Tuna) -> true
+  | (_, _) -> false
+
+(eqFish : (Fish * Fish) -> bool)
+
+// 40
+let rec removeFish'(f: Fish, p: FishPizza) =
+  match p with
+  | Bottom -> Bottom
+  | Topping(t, p) -> if eqFish(f, t) then removeFish'(f, p) else Topping(t, removeFish'(f, p))
+
+// 57
+let rec removeInt(x: int, p: Pizza<int>) =
+  match p with
+  | Bottom -> Bottom
+  | Topping(y, p) -> if x = y then removeInt(x, p) else Topping(y, removeInt(x, p))
